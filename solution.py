d14cd5a6670aeed76e55345952bf35c542003fe7
Áron Artúr Nagy <bigaron2@gmail.com>
2023-11-22 20:05:10 +0100

    second task is done

--- solution.py
+++ solution.py
@@ -13,43 +13,48 @@ def get_entropy(n_cat1: int, n_cat2: int) -> float:
 
     return entropy
 
+
+def get_min_max(features: list, col: int) -> (int, int):
+    min_val = max_val = features[0][col]
+    for row in features:
+        if row[col] > max_val: max_val = row[col]
+        elif row[col] < min_val: min_val = row[col]
+
+    return min_val, max_val
+
 ###################### 2. feladat, optimális szeparáció #######################
 def get_best_separation(features: list, labels: list) -> (int, int):
     best_separation_feature, best_separation_value = 0, 0
-    e, f = 0, 0
-    e0 = 0
-    e1 = 0
-    f0 = 0
-    f1 = 0
-    a = 0
-    b0 = 0
-    b1 = 0
+    base_labels = [0, 0]
+    for cntr in range(labels.size):
+        base_labels[labels[cntr]] += 1
 
-    for i in range(features.shape[1]):
-        if labels[i] == 0: b0 += 1
-        else: b1 += 1
+    base_entropy = get_entropy(base_labels[0], base_labels[1])
 
-    base_entropy = get_entropy(b0, b1)
+    best_separation_value = 0
+    current_best_info_gain = 0
 
-    for i in range(features.shape[1]):
-        cntr = 0
-        for row in features:
-            a = row[i]
-            for sep in features:
-                if sep[i] <= a: 
+    for col in range(features.shape[1]):
+        (min_a, max_a) = get_min_max(features, col)
+
+        for a_iterator in range(min_a, max_a, 1):
+            e_labels = [0, 0]
+            f_labels = [0, 0]
+            e = 0
+            f = 0
+            for second_scan in range(features.shape[0]):
+                if features[second_scan][col] <= a_iterator:
                     e += 1
-                    if labels[cntr] == 0: e0 += 1
-                    else: e1 += 1
-                else: 
+                    e_labels[labels[second_scan]] += 1
+                else:
                     f += 1
-                    if labels[cntr] == 0: f0 += 1
-                    else: f1 += 1
-            info_win = base_entropy - (e / (e+f) * get_entropy(e0, e1) + f/(f+e) * get_entropy(f0, f1))    
-            if info_win > best_separation_value:
-                best_separation_value = info_win
-                best_separation_feature = i
-            cntr += 1
-        
+                    f_labels[labels[second_scan]] += 1
+            current_info_gain = base_entropy - (e / (e+f) * get_entropy(e_labels[0], e_labels[1]) + f/(e+f) * get_entropy(f_labels[0], f_labels[1])) 
+            if current_info_gain > current_best_info_gain:
+                current_best_info_gain = current_info_gain
+                best_separation_value = a_iterator
+                best_separation_feature = col
+
     return best_separation_feature, best_separation_value
 
 ################### 3. feladat, döntési fa implementációja ####################
